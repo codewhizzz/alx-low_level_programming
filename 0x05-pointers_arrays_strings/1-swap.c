86ee8423f00b257a2afe41a5760262bfefc9023f
Damilare <damilaretoyese@gmail.com>
2022-12-19 17:36:08 -0800

    pointer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,26 +1,17 @@
-julien@ubuntu:~/0x05$ cat 1-main.c
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * swap_int - a function that swaps the values of two integers.
+ * @a: first value to be provided
+ * @b: Second parameter to be provided
+ * int c: addressed the swapped value will be sent to
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-    int a;
-    int b;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
-julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-swap.c -o 1-swap
-julien@ubuntu:~/0x05$ ./1-swap 
-a=98, b=42
-a=42, b=98
-julien@ubuntu:~/0x05$
